7b4f9001eac08cc08cb860040fb9274710732735
KennedyShiku <kenrichshiku@gmail.com>
2023-12-08 14:02:17 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
+# sequences.py
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print('[]')
+    elif length == 1:
+        print('[0]')
+    else:
+        fib_sequence = [0, 1]
+        for i in range(2, length):
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        print(f'[{", ".join(map(str, fib_sequence))}]')
